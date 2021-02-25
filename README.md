80cd2936dc97db46a5610472a7a2cb8fcda93491
Arun <marun790@gmail.com>
2021-02-25 16:24:55 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # angular-springboot_country
 ## this project using frontend as angular backend as springboot
+#### Technical specifications
 
+**Stack**|**Version**
+---------|-----------
+Java        | 8.0
+spring-boot | 
---
+++ country-services/src/test/java/com/arun/demo/countryservices/CountryServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arun.demo.countryservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
